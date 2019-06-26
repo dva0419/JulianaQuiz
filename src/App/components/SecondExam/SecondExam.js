432bbad7d1e6eab021085e7ade643d391357986c
dva0419 <david_velasquez82142@elpoli.edu.co>
2019-06-26 12:22:23 -0500

    quiz2

---
+++ src/App/components/SecondExam/SecondExam.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+
+const About = () => {
+    return (
+        <React.Fragment>
+
+            <h1 className="animated fadeIn fast">Segundo examen (16%)</h1>
+            <hr />
+            <p className="animated fadeIn">
+                <p><h2 className="animated fadeIn fast">Indicaciones:</h2></p>
+                <div>
+                    <ol>
+                        <li>Descargar todo el proyecto de Heroes-React.</li>
+                        <li>En este espacio, usted debe crear una página que tenga que ver con su proyecto de clase.</li>
+                        <li>Entre los integrantes del proyecto no pueden existir paginas iguales, ni similares.</li>
+                        <li>El proyecto completo debe adicionarse al GitHub.</li>
+                        <li>En un documento ubicado en el Google Drive cada uno escribirá el enlace suministrado por GitHub para una posterior descarga.</li>
+                        <li>El contenido de la página como mínimo debe tener algo similar a la página de "Heroes"; es decir, varias imagenes, enlaces, divisiones, lo que usted considere pertinente para ganarse un 5.0.</li>
+                        <li>A las 12 m. del día de hoy debe estar el enlace en el Google Drive.</li>
+                    </ol>
+                </div>
+            </p>
+            <div className="jumbotron jumbotron-fluid animated fadeIn fast">
+            <div className="container">
+                
+          
+             <h1 className="dispaly-3">Complejo deportivo</h1>
+             
+            <hr />
+            <p className="animated fadeIn">
+                <p><h2 className="lead">¿De que trata nuestro proyecto?</h2></p>
+                
+                <p>Somos una pagina web dispuesta a brindar una mejor información y manejo de los espacios deportivos de nuestro complejo, para el aprovechamiento adecuado del mismo. Tenemos muchas opciones para ti  </p>
+                    <div>
+                        <ol>
+                            <li>Reservas de los espacios deportivos.</li>
+                            
+                                <li>Información de cada uno de los escenarios </li>
+                                    <li>Fotos </li> 
+                                    <li> Y mucha mas información de tu interes </li>
+                </ol>
+          
+
+               
+            </div>
+            </p>
+                </div>
+        </div>
+        
+        <button type="button" className="btn btn-outline-primary btn-block">
+            Ver más...
+        </button>
+    
+
+        </React.Fragment>
+    )
+    
+}
+
+export default About
